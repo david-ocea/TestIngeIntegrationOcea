c7a86ae11918c9f193f26d96f1bdb4ea11275663
Jordan-Touly <jordan.touly@hotmail.fr>
2026-01-22 15:30:42 +0100

    feat: Ajout des champs géocodage et récupération via API des coordonnées GPS

---
+++ src/TestIngeIntegrationOceaConsole/Services/GeoResult.cs
@@ -0,0 +1,12 @@
+namespace OceaSmartBuildingApp
+{
+    /// <summary>
+    /// Représente les coordonnées GPS
+    /// </summary>
+    public class GeoResult
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Score { get; set; }
+    }
+}
